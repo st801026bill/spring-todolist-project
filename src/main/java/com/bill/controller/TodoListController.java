54fc0e733c7e1c53f1a01fa55933c13d88ba5b9b
bill.chou <bill.chou@nogle.com>
2022-08-16 14:49:37 +0800

    change http method

--- src/main/java/com/bill/controller/TodoListController.java
+++ src/main/java/com/bill/controller/TodoListController.java
@@ -11,9 +11,11 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,14 +33,14 @@ public class TodoListController {
     }
 
     @Operation(summary = "代辦事項修改", description = "代辦事項修改")
-    @PostMapping("/todo/update")
+    @PutMapping("/todo/update")
     public String updateTodo(@RequestBody TodoListUpdateReqDto reqDto) {
         service.updateTodoList(reqDto);
         return "send message success";
     }
 
     @Operation(summary = "代辦事項刪除", description = "代辦事項刪除")
-    @PostMapping("/todo/delete")
+    @DeleteMapping("/todo/delete")
     public String deleteTodo(@RequestBody TodoListDeleteReqDto reqDto) {
         service.deleteTodoList(reqDto);
         return "send message success";
